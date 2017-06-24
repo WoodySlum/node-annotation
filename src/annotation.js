95ce159706be0a6025e26d930feb84059b9ecdc8
smizrahi <smizrahi@v3d.fr>
2017-06-24 21:14:56 +0200

    Change file import to class string

--- src/annotation.js
+++ src/annotation.js
@@ -1,21 +1,12 @@
 var AnnotationReader = require('./Reader/Annotation.js');
 var AnnotationParser = require('./Parser/Annotation.js');
-var FileReader       = require('./Reader/File.js');
 
-var fileReader = new FileReader();
 var annotationParser = new AnnotationParser();
 
-var annotation = function(path, callback) {
-
-    fileReader.read(path, function(err, result) {
-        if (err) {
-            console.log(err);
-        }
-
-        annotationParser.parse(result, function(comments) {
+var annotation = function(classString, callback) {
+        annotationParser.parse(classString, function(comments) {
             callback(new AnnotationReader(comments));
         });
-    });
 }
 
 module.exports = annotation;
